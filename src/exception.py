3f746161a8c4f5a2cb187c502233974e04f6e8d0
jevpau1 <pauljerry060@gmail.com>
2023-09-03 10:55:56 +0400

    Logging and Exception Handling

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+def error_message_detail(error,error_detail):
+    _,_,exec_tb=error_detail.exc_info()
+    filename=exec_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name[{0}] in line number [{1}] with error message[{2}]".format(
+    filename,exec_tb.tb_lineno,str(error))
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail):
+        super(CustomException,self).__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e,sys)
